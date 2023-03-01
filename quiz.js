cce8286222a1b6a20e14e57e8f55629d220c6211
marcelis <marcelis123@wp.pl>
2023-03-01 10:29:40 +0100

    working with quiz

--- quiz.js
+++ quiz.js
@@ -3,21 +3,44 @@ const sel = $("#select");
 const qu1 = document.querySelector("#q1");
 const qu2 = document.querySelector("#qlist")
 let answer = document.querySelectorAll(".ans");
+const txt = document.querySelector("h1");
+const submit = document.querySelector("#submit");
+
+const quiz = [
+    { 
+        question: "test",
+        ans1: "one",
+        ans2: "two",
+        ans3: "three",
+},
+
+]
 
 
 
+
+submit.addEventListener("click", submitted);
 easy.addEventListener("click", easyQuiz);
 
 answer.forEach(answerr => { 
     answerr.addEventListener("click", (e)=> { 
-        document.querySelector("h1").innerHTML = e.target.value;
+        txt.innerHTML = e.target.value;
     })
 })
 
 function easyQuiz() { 
     $(".diff").fadeOut(500);  
     sel.fadeOut(500);
-    qu1.classList.remove("hdn");
-    qu2.classList.remove("hdn");
+    qu1.classList.remove("hidden");
+    qu2.classList.remove("hidden");
+    qu1.classList.add("flex");
+    qu2.classList.add("flex");
     
+}
+function submitted(){ 
+    if(txt.innerHTML == "true"){ 
+        console.log("bro got one poin");
+    } else { 
+        console.log("bruh");
+    }
 }
